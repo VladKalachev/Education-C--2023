b8323ec5edc97fbbc114056327d3bcc13966cd8e
Corvax <vlvete1990@yandex.ru>
2023-02-07 21:38:35 +0300

    chore: added int value

--- CSLight/CSLight/Program.cs
+++ CSLight/CSLight/Program.cs
@@ -11,6 +11,24 @@ namespace CSLight
         static void Main(string[] args)
         {
             Console.WriteLine("Привет, Мир!");
+            Console.ReadKey();
+
+            // Основные типы
+            // Целочисленные типы int
+
+            byte a;    // 0 до 255
+            sbyte b;    // -128 до 127
+            short s;    // -32768 до 32767
+            ushort u;   // 0 до 65535
+            int i;     // -2147483648 до 2147483647
+            uint i2;  // 0 до 4294672295
+            long l = long.MaxValue;
+            ulong l2; //
+
+            // Числа с плавающей точкой
+            float f2;
+            double d2;
+
         }
     }
 }
